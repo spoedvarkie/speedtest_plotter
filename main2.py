1542855b469baf4100747ea35926197fd28fa947
Christo du Plessis <duplessis.christo3616@gmail.com>
2021-08-26 22:05:35 +0200

    main2 updates

--- main2.py
+++ main2.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 df = pd.read_csv('results.csv')
 
-fig = px.line(df, x='time', y='curr_ping', title='Speedtest Stats')
-fig.add_scatter(x=df['time'], y=df['curr_upload'], name='Upload')
-fig.add_scatter(x=df['time'], y=df['curr_download'], name='Download')
-fig.add_scatter(x=df['time'], y=df['avg_ping'], name='Avg Ping')
-fig.add_scatter(x=df['time'], y=df['avg_upload_speed']/1000000, name='Avg Upload')
-fig.add_scatter(x=df['time'], y=df['avg_download_speed']/1000000, name='Avg Download')
+fig = px.line(df, x='time', y=['curr_ping', 'curr_upload', 'curr_download', 'avg_ping', 'avg_upload_speed',
+                               'avg_download_speed'], title='Speedtest Stats', markers=True)
+# fig.add_scatter(x=df['time'], y=df['curr_upload'], name='Upload')
+# fig.add_scatter(x=df['time'], y=df['curr_download'], name='Download')
+# fig.add_scatter(x=df['time'], y=df['avg_ping'], name='Avg Ping')
+# fig.add_scatter(x=df['time'], y=df['avg_upload_speed']/1000000, name='Avg Upload')
+# fig.add_scatter(x=df['time'], y=df['avg_download_speed']/1000000, name='Avg Download')
+
 fig.update_xaxes(
     rangeslider_visible=True,
     rangeselector=dict(
