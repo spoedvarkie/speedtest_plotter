6bf453c21ed21de10b3d61d33f18f60b2efe3ebc
Christo du Plessis <duplessis.christo3616@gmail.com>
2021-08-26 21:51:47 +0200

    main2.py added with plotly.express requirements updated copy pasted some results for more data

---
+++ main2.py
@@ -0,0 +1,26 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('results.csv')
+
+fig = px.line(df, x='time', y='curr_ping', title='Speedtest Stats')
+fig.add_scatter(x=df['time'], y=df['curr_upload'], name='Upload')
+fig.add_scatter(x=df['time'], y=df['curr_download'], name='Download')
+fig.add_scatter(x=df['time'], y=df['avg_ping'], name='Avg Ping')
+fig.add_scatter(x=df['time'], y=df['avg_upload_speed']/1000000, name='Avg Upload')
+fig.add_scatter(x=df['time'], y=df['avg_download_speed']/1000000, name='Avg Download')
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1y <", step="year", stepmode="backward"),
+            dict(count=6, label="6m <", step="month", stepmode="backward"),
+            dict(count=1, label="1m <", step="month", stepmode="backward"),
+            dict(count=1, label="Now", step="day", stepmode="todate"),
+            dict(count=1, label="Now Month", step="month", stepmode="todate"),
+            dict(count=1, label="Now Year", step="year", stepmode="todate"),
+            dict(step="all")
+        ])
+    )
+)
+fig.show()
